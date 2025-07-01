2b2d484744cfe59d36c1d4dc7bd1915430ef30dd
MarcosVel <marcosvelosodev@gmail.com>
2025-07-01 00:53:01 -0300

    Initial Commit

---
+++ Threads/ThreadsApp.swift
@@ -0,0 +1,17 @@
+//
+//  ThreadsApp.swift
+//  Threads
+//
+//  Created by Marcos Roberto Lemos Veloso on 01/07/25.
+//
+
+import SwiftUI
+
+@main
+struct ThreadsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
