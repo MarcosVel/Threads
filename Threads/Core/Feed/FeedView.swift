81a40bcd4b578f5b31c2019db5c92a414bfe97be
MarcosVel <marcosvelosodev@gmail.com>
2025-07-01 19:51:27 -0300

    feat: feed cell component

--- Threads/Core/Feed/FeedView.swift
+++ Threads/Core/Feed/FeedView.swift
@@ -9,7 +9,15 @@ import SwiftUI
 
 struct FeedView: View {
     var body: some View {
-        Text("Hello, Feed!")
+        NavigationStack {
+            ScrollView(showsIndicators: false) {
+                LazyVStack {
+                    ForEach(0..<20, id: \.self) { thread in
+                        ThreadCell()
+                    }
+                }
+            }
+        }
     }
 }
 
