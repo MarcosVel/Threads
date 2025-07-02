805593a6e5611464d53514ce2874acf566639da7
MarcosVel <marcosvelosodev@gmail.com>
2025-07-01 21:21:14 -0300

    feat: search user component

--- Threads/Core/Explore/ExploreView.swift
+++ Threads/Core/Explore/ExploreView.swift
@@ -13,9 +13,55 @@ struct ExploreView: View {
         NavigationStack {
             ScrollView {
                 LazyVStack {
+                    ForEach(0...10, id: \.self) { user in
+                        VStack {
+                            HStack {
+                                AsyncImage(
+                                    url: URL(
+                                        string:
+                                            "https://github.com/MarcosVel.png")
+                                ) {
+                                    result in
+                                    result.image?
+                                        .resizable()
+                                        .scaledToFill()
+                                }
+                                .frame(width: 40, height: 40)
+                                .clipShape(.circle)
 
+                                VStack(alignment: .leading) {
+                                    Text("marcosveloso99")
+                                        .fontWeight(.semibold)
+
+                                    Text("Marcos Veloso")
+                                }
+                                .font(.footnote)
+
+                                Spacer()
+
+                                Button {
+
+                                } label: {
+                                    Text("Follow")
+                                        .font(.subheadline)
+                                        .fontWeight(.semibold)
+                                        .foregroundStyle(.black)
+                                }
+                                .frame(width: 100, height: 32)
+                                .overlay {
+                                    RoundedRectangle(cornerRadius: 10)
+                                        .stroke(
+                                            Color(.systemGray4), lineWidth: 1)
+                                }
+                            }
+                            .padding(.horizontal)
+                        }
+                        Divider()
+                    }
+                    .padding(.vertical, 4)
                 }
             }
+            .navigationTitle("Search")
             .searchable(text: $searchText, prompt: "Search")
         }
     }
