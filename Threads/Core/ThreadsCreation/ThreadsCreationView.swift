274dd6e6c53d58b9a2d37c91eeabee815f99f719
MarcosVel <marcosvelosodev@gmail.com>
2025-07-24 02:12:21 -0300

    feat[WIP]: creation thread header

--- Threads/Core/ThreadsCreation/ThreadsCreationView.swift
+++ Threads/Core/ThreadsCreation/ThreadsCreationView.swift
@@ -9,7 +9,27 @@ import SwiftUI
 
 struct ThreadsCreationView: View {
     var body: some View {
-        Text("ThreadsCreation!")
+        NavigationStack {
+            VStack {
+
+            }
+            .navigationTitle("New Thread")
+            .navigationBarTitleDisplayMode(.inline)
+            .toolbar {
+                ToolbarItem(placement: .topBarLeading) {
+                    Button("Cancel") {}
+                        .font(.subheadline)
+                        .foregroundStyle(.black)
+                }
+
+                ToolbarItem(placement: .topBarTrailing) {
+                    Button("Post") {}
+                        .font(.subheadline)
+                        .fontWeight(.semibold)
+                        .foregroundStyle(.black)
+                }
+            }
+        }
     }
 }
 
