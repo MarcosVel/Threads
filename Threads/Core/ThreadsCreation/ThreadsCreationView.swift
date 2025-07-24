81b69a07d9608148580e1d9f4a69847f4ddc07ad
MarcosVel <marcosvelosodev@gmail.com>
2025-07-24 02:18:13 -0300

    feat[WIP]: input to post a thread

--- Threads/Core/ThreadsCreation/ThreadsCreationView.swift
+++ Threads/Core/ThreadsCreation/ThreadsCreationView.swift
@@ -8,25 +8,60 @@
 import SwiftUI
 
 struct ThreadsCreationView: View {
+    @State private var caption: String = ""
     var body: some View {
         NavigationStack {
             VStack {
+                HStack(alignment: .top) {
+                    CircularProfileImgView()
 
+                    VStack(alignment: .leading, spacing: 4) {
+                        Text("marcos.veloso")
+                            .fontWeight(.semibold)
+
+                        TextField(
+                            "Start a thread...", text: $caption, axis: .vertical
+                        )
+                    }
+                    .font(.footnote)
+
+                    Spacer()
+
+                    if !caption.isEmpty {
+                        Button {
+                            // clears the input
+                            caption = ""
+                        } label: {
+                            Image(systemName: "xmark")
+                                .resizable()
+                                .frame(width: 12, height: 12)
+                                .foregroundStyle(.gray)
+                        }
+                    }
+                }
+                Spacer()
             }
+            .padding()
             .navigationTitle("New Thread")
             .navigationBarTitleDisplayMode(.inline)
             .toolbar {
                 ToolbarItem(placement: .topBarLeading) {
-                    Button("Cancel") {}
-                        .font(.subheadline)
-                        .foregroundStyle(.black)
+                    Button("Cancel") {
+
+                    }
+                    .font(.subheadline)
+                    .foregroundStyle(.black)
                 }
 
                 ToolbarItem(placement: .topBarTrailing) {
-                    Button("Post") {}
-                        .font(.subheadline)
-                        .fontWeight(.semibold)
-                        .foregroundStyle(.black)
+                    Button("Post") {
+
+                    }
+                    .font(.subheadline)
+                    .fontWeight(.semibold)
+                    .foregroundStyle(.black)
+                    .opacity(caption.isEmpty ? 0.5 : 1)
+                    .disabled(caption.isEmpty)
                 }
             }
         }
