1710d0233b4fc90b4cc9066b30fa66e41e98de45
MarcosVel <marcosvelosodev@gmail.com>
2025-07-24 21:33:16 -0300

    chore: configuring firebase

--- Threads/App/ThreadsApp.swift
+++ Threads/App/ThreadsApp.swift
@@ -5,10 +5,24 @@
 //  Created by Marcos Roberto Lemos Veloso on 01/07/25.
 //
 
+import FirebaseCore
 import SwiftUI
+
+class AppDelegate: NSObject, UIApplicationDelegate {
+    func application(
+        _ application: UIApplication,
+        didFinishLaunchingWithOptions launchOptions: [UIApplication
+            .LaunchOptionsKey: Any]? = nil
+    ) -> Bool {
+        FirebaseApp.configure()
+        return true
+    }
+}
 
 @main
 struct ThreadsApp: App {
+    @UIApplicationDelegateAdaptor(AppDelegate.self) var delegate
+
     var body: some Scene {
         WindowGroup {
             ContentView()
